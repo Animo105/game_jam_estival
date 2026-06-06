55aa47999d51a13e39389b4566ad7d133dde455b
perceval001 <156114624+perceval001@users.noreply.github.com>
2026-06-06 19:08:35 -0400

    tkt y'aura pas de merge conflict

--- Enemy/demon_enemy.gd
+++ Enemy/demon_enemy.gd
@@ -1,1 +1,14 @@
 extends BasicEnemy
+@onready var stun: Timer = $Stun
+
+
+func _on_area_3d_body_entered(body: Node3D) -> void:
+	if body is Player:
+		Globals.health -= 1
+		var direction : Vector3 = (body.global_position - global_position).normalized()
+		direction *= 40
+		direction.y = 2
+		body.linear_velocity += direction
+		print("playerHP : " + str(Globals.health))
+		state = States.HIT
+		stun.start()
