f4b820e31a43aa70dd0be78a74d141ba55fb7e35
Roket <roket8274@gmail.com>
2026-06-07 02:01:55 -0400

    level 2

--- Enemy/Scripts/jonny.gd
+++ Enemy/Scripts/jonny.gd
@@ -5,8 +5,3 @@ func _physics_process(_delta):
 		naviguate()
 	elif state == States.HIT:
 		hit_recoil()
-
-func handle_altitude():
-	if global_position.y < 1:
-		print("up")
-		apply_force(Vector3.UP * 5)
