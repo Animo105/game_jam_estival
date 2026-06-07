055fe93bfca75d6abf316110ff6f1fd45006ea5b
Roket <roket8274@gmail.com>
2026-06-07 04:05:53 -0400

    Health bar

---
+++ Prefabs/lava.gd
@@ -0,0 +1,21 @@
+extends Node3D
+@onready var mesh: MeshInstance3D = $Mesh
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	var material := mesh.get_active_material(0) as StandardMaterial3D
+	if material:
+		material.uv1_offset += Vector3(delta * 0.02, delta * 0.02, delta * 0.02)
+
+
+func _on_area_3d_body_entered(body: Node3D) -> void:
+	var player = body as Player
+	
+	if player:
+		player.linear_velocity.y += 10.0
