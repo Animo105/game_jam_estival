707a3a01b6792aa67981245ee5f4b77f09ce2801
Animo105 <emeric2005leclerc@gmail.com>
2026-06-06 21:55:05 -0400

    making the code compile

--- Globals/Autoloads/swap_manager.gd
+++ Globals/Autoloads/swap_manager.gd
@@ -7,9 +7,9 @@ const DAEMON_SWAP_MIN_TIME : int = 10
 
 var do_swap_timer : bool = false
 var can_swap : bool = true
-var life_gain_timer : Timer
-var office_swap_timer : Timer
-var daemon_swap_timer : Timer
+var life_gain_timer : Timer = Timer.new()
+var office_swap_timer : Timer = Timer.new()
+var daemon_swap_timer : Timer = Timer.new()
 
 func _ready() -> void:
 	life_gain_timer.wait_time = REGEN_RATE
