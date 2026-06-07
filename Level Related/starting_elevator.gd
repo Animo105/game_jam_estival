8390b88c4c6d0f03888c190f6ce340cd1d9308ae
Animo105 <emeric2005leclerc@gmail.com>
2026-06-07 13:26:50 -0400

    Levers reset

--- Level Related/starting_elevator.gd
+++ Level Related/starting_elevator.gd
@@ -4,8 +4,9 @@ class_name StartingElevator
 @onready var door_shape: CollisionShape3D = $ElevatorDoor/DoorShape
 @onready var elevator_mesh: MeshInstance3D = $Elevator/ElevatorMesh
 @onready var count_down_label: Label3D = $count_down
-var tween : Tween
+@onready var collision_shape_3d: CollisionShape3D = $StaticBody3D/CollisionShape3D
 
+var tween : Tween
 var count_down : int = 3:
 	set(value):
 		count_down = value
@@ -19,6 +20,7 @@ func open_door():
 	tween = create_tween()
 	tween.tween_property(Globals.player, "health", Player.STARTING_LIFE, 2)
 	tween.tween_property(self, "count_down", 0, 3)
+	tween.tween_property(collision_shape_3d, "disabled", true, 0)
 	tween.tween_property(elevator_mesh, "blend_shapes/Open", 1, 0.3)
 	await tween.finished
 	ElevatorAudioStreamPlayer.exit_elevator()
